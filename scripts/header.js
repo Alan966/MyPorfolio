cef42eb5bc799e110692847d67aad0b7a21e7feb
Alan Jair Guadalupe Marinez <66544589+Alan966@users.noreply.github.com>
2023-03-27 10:04:38 -0600

    Include index.html and scripts and .gitignore

--- scripts/header.js
+++
@@ -1,9 +0,0 @@
-const button_hamburguer = document.getElementById("content_2");
-const line = document.createElement("div");
-line.classList.add("line");
-
-button_hamburguer.insertAdjacentElement("afterbegin", line);
-
-button_hamburguer.addEventListener("click", () => {
-    button_hamburguer.classList.toggle("active");
-})
